0256a568cf4c03fd9806f8cbbf2a5aac81f49cc5
Tracy Tran <61801256+enobot@users.noreply.github.com>
2021-07-28 12:17:17 -0700

    Time: 9095 ms (5.01%), Space: 40.4 MB (34.06%) - LeetHub

---
+++ implement-strstr/implement-strstr.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let indexOfHaystack = 0;
+    
+    if (needle.length > haystack.length) return -1;
+    while (indexOfHaystack < haystack.length) {
+        let index = indexOfHaystack;
+        let count = 0;
+        if (haystack[index] === needle[count]) {
+            while (haystack[index] === needle[count] && index < haystack.length) {
+                count++;
+                index++;
+            }
+            if (count === needle.length) return indexOfHaystack;
+        } indexOfHaystack++;
+    }
+    
+    return needle.length === 0 ? 0 : -1;
+};
