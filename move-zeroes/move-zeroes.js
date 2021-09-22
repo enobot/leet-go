c70bd1c333d8abee097798099daee87b729c5779
Tracy Tran <61801256+enobot@users.noreply.github.com>
2021-09-21 20:57:33 -0700

    Time: 84 ms (61.13%), Space: 40.3 MB (70.33%) - LeetHub

---
+++ move-zeroes/move-zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let lastNonZeroIdx = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) nums[lastNonZeroIdx++] = nums[i];
+    }
+    
+    for (let j = lastNonZeroIdx; j < nums.length; j++) {
+        nums[j] = 0;
+    }
+    
+    return nums;
+};
