bfeb3cc72f6fc4018e61b3243ba6d156e3e10169
Tracy Tran <61801256+enobot@users.noreply.github.com>
2021-10-04 12:55:22 -0700

    Time: 125 ms (28.42%), Space: 47.7 MB (87.70%) - LeetHub

--- container-with-most-water/container-with-most-water.js
+++ container-with-most-water/container-with-most-water.js
@@ -4,15 +4,13 @@
  */
 var maxArea = function(height) {
     let mostWater = 0;
-    let left = 0;
-    let right = height.length - 1;
+    let left = 0, right = height.length - 1;
     
     while (left < right) {
-        let currentContainer = (right - left) * Math.min(height[left], height[right]);
-        mostWater = Math.max(currentContainer, mostWater);
-        
-        if (height[right] > height[left]) left++;
-        else right--;
+        let currentWater = (right - left) * Math.min(height[right], height[left]);
+        mostWater = Math.max(mostWater, currentWater);
+        height[left] < height[right] ? left++ : right--;
     }
+    
     return mostWater;
 };
