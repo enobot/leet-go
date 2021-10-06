a04701b81a44b8fb377a2f3f481b0b03e3ec8059
Tracy Tran <61801256+enobot@users.noreply.github.com>
2021-10-06 14:50:23 -0700

    Time: 136 ms (92.13%), Space: 49.4 MB (48.06%) - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -3,27 +3,34 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    if (nums.length < 3) return [];
-    let triplets = [];
-    nums.sort((a,b) => a - b)
+    const triplets = [];
+    nums.sort((a,b) => a-b);
     
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let i = 0; i < nums.length; i++) {
         let left = i + 1;
         let right = nums.length - 1;
+        
         
         while (left < right) {
             let sum = nums[i] + nums[left] + nums[right];
             if (sum === 0) {
-                triplets.push([nums[i], nums[left], nums[right]]);
+                triplets.push([nums[i], nums[left], nums[right]])
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
                 left++;
                 right--;
-                while (left < right && nums[left] === nums[left - 1]) left++;
-                while (left < right && nums[right] === nums[right + 1]) right--;
-            }
-            if (sum > 0) right--;
-            if (sum < 0) left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }            
+        }
+
+        
+        while (i < nums.length - 1 && nums[i] === nums[i + 1]) {
+            i++;
         }
     }
+    
     return triplets;
 };
