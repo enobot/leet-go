8d96aaf31729de7ea59a7c1b09cc9f92fe30c717
Tracy Tran <61801256+enobot@users.noreply.github.com>
2021-10-06 19:19:09 -0700

    Time: 96 ms (47.28%), Space: 49 MB (5.82%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let currentMax = nums[0];
     let currentSum = nums[0];
+    let maxSum = nums[0];
     
     for (let i = 1; i < nums.length; i++) {
-        currentSum = Math.max(nums[i], currentSum + nums[i])
-        if (currentSum > currentMax) currentMax = currentSum;
+        currentSum = Math.max(nums[i], nums[i] + currentSum)
+        maxSum = Math.max(currentSum, maxSum)
     }
     
-    return currentMax;
+    return maxSum;
 };
