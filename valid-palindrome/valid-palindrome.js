6377895b08a0df5e1f84c5ce1add6019c8ef49a9
Tracy Tran <61801256+enobot@users.noreply.github.com>
2021-10-08 12:43:12 -0700

    Time: 92 ms (62.82%), Space: 40.8 MB (76.26%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.replace(/[^0-9a-zA-Z]+/gmi,"").toLowerCase();
+    
+    let left = 0, right = s.length - 1;
+    
+    while (left < right) {
+        if (s[left] !== s[right]) return false;
+        left++;
+        right--;
+    }
+    
+    return true;
+};
